6e8f1ac10d8d272990238b8a10f81d88f748109a
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-09-20 23:14:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  index = 0 
+  new_squared_array = []
+  while index < array.length do
+    element = array[index]
+    squared_element = element**
+    new_squared_array.push(squared_element)
+    index +=1
+  end
+  new_squared_array
 end
